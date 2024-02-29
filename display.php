788f6511160b2e171ed15d9c5f9881f55fdf2fe7
Igitoro <84076628+Igitoro@users.noreply.github.com>
2024-02-29 11:54:11 +0300

    Update display.php

--- display.php
+++ display.php
@@ -1,5 +1,6 @@
 <?php
-$name = $_POST["firstname"];
-$surname = $_POST["lastname"];
-echo "Ваше имя: <b>". $name . " " . $surname . "</b>";
+if(isset($_POST['firstname']) && isset($_POST['lastname'])){
+  $name = $_POST["firstname"];
+  $surname = $_POST["lastname"];
+  echo "Ваше имя: <b>". $name . " " . $surname . "</b>";
 ?>
